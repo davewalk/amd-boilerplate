030b698bbd2801de241a4ceac88bc062d3db156a
Dave Walk <daviddwalk@gmail.com>
2013-06-16 00:14:03 -0400

    Started building server Grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,27 @@
 module.exports = function (grunt) {
 	// Project configuration
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json')
-
-
-
-
+		pkg: grunt.file.readJSON('package.json'),
+		port: 9000,
+		connect: {
+			server: {
+				options: {
+					port: 9000,
+					keepalive: true,
+					base: 'app/'
+				}
+			}			
+		},
+		open: {
+			dev: {
+				path: 'http://localhost:<%= connect.server.options.port %>'
+			}
+		}
 	});
+
+	grunt.registerTask('server', ['open:dev','connect']);
+
+	grunt.loadNpmTasks('grunt-open');
+	grunt.loadNpmTasks('grunt-contrib-watch');	
+	grunt.loadNpmTasks('grunt-contrib-connect');	
 };
